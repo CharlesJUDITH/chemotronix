cef5255e3cd6e5c1867be514b81023ef96bb29c7
Charles JUDITH <contact@charlesjudith.com>
2022-07-04 11:29:12 +0200

    add cosmos chain

---
+++ x/chemotronix/genesis_test.go
@@ -0,0 +1,29 @@
+package chemotronix_test
+
+import (
+	"testing"
+
+	keepertest "chemotronix/testutil/keeper"
+	"chemotronix/testutil/nullify"
+	"chemotronix/x/chemotronix"
+	"chemotronix/x/chemotronix/types"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGenesis(t *testing.T) {
+	genesisState := types.GenesisState{
+		Params: types.DefaultParams(),
+
+		// this line is used by starport scaffolding # genesis/test/state
+	}
+
+	k, ctx := keepertest.ChemotronixKeeper(t)
+	chemotronix.InitGenesis(ctx, *k, genesisState)
+	got := chemotronix.ExportGenesis(ctx, *k)
+	require.NotNil(t, got)
+
+	nullify.Fill(&genesisState)
+	nullify.Fill(got)
+
+	// this line is used by starport scaffolding # genesis/test/assert
+}
